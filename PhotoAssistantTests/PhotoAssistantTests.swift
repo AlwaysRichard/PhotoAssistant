0e76ce67006ee1d36f660d6eb0648667e5e75a15
Richard Cox <AlwaysRichard@gmail.com>
2025-10-13 14:04:39 -0400

    Initial Commit

---
+++ PhotoAssistantTests/PhotoAssistantTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoAssistantTests.swift
+//  PhotoAssistantTests
+//
+//  Created by Richard Cox on 10/13/25.
+//
+
+import Testing
+
+struct PhotoAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
